e355e92154c4490e673d11d1369708837b4d7fd2
Dominique Gigon <dgigon@jahia.com>
2022-11-18 09:33:28 +0100

    SUPPORT-193 added cfg configuration for api key

--- src/main/java/org/jahia/community/translation/deepl/service/impl/DeepLTranslatorServiceImpl.java
+++ src/main/java/org/jahia/community/translation/deepl/service/impl/DeepLTranslatorServiceImpl.java
@@ -7,6 +7,7 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.SocketAddress;
 import java.time.Duration;
+import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -21,17 +22,17 @@ import org.jahia.services.content.LazyPropertyIterator;
 import org.jahia.services.content.nodetypes.ExtendedPropertyDefinition;
 import org.jahia.settings.SettingsBean;
 import org.jahia.utils.LanguageCodeConverters;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DeepLTranslatorServiceImpl implements DeepLTranslatorService {
+@Component(service = {ManagedService.class}, property = "service.pid=org.jahia.community.translationdeepl", immediate = true)
+public class DeepLTranslatorServiceImpl implements DeepLTranslatorService, ManagedService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DeepLTranslatorServiceImpl.class);
-    private final String authKey;
-
-    public DeepLTranslatorServiceImpl() {
-        authKey = SettingsBean.getInstance().getPropertyValue("translation.deepl.api.key");
-    }
+    private String authKey;
 
     @Override
     public void translate(String path, String srcLanguage, String destLanguage) {
@@ -90,4 +91,14 @@ public class DeepLTranslatorServiceImpl implements DeepLTranslatorService {
         }
     }
 
+    @Override
+    public void updated(Dictionary<String, ?> dictionary) throws ConfigurationException {
+        if(dictionary != null)
+            authKey = (String) dictionary.get("translation.deepl.api.key");
+        if(!(authKey != null && !authKey.trim().isEmpty()))
+            authKey = SettingsBean.getInstance().getPropertyValue("translation.deepl.api.key");
+        if(!(authKey != null && !authKey.trim().isEmpty()))
+            LOGGER.error("translation.deepl.api.key not defined");
+        LOGGER.debug("translation.deepl.api.key = {}",authKey);
+    }
 }
