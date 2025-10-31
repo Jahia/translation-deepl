2ec52cd9db9a71f8a15ea5045de9a579867f5631
Florent BOURASSE <fbourasse@jahia.com>
2025-10-31 17:00:08 +0100

    feature: addition of an icon
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/javascript/RequestTranslationDeepl/register.jsx
+++ src/javascript/RequestTranslationDeepl/register.jsx
@@ -2,31 +2,25 @@ import React from "react";
 import {
     registry
 } from "@jahia/ui-extender";
-import {Copy, Lock} from "@jahia/moonstone";
+
 import {
     RequestTranslationDeeplForAllLanguagesActionComponent
 } from "./RequestTranslationDeeplForAllLanguagesActionComponent";
 import {RequestTranslationDeeplActionComponent} from "./RequestTranslationDeeplActionComponent";
+import DeeplIcon from "./DeeplIcon";
 
 export default () => {
     registry.add('action', 'requestTranslationDeepl', {
-        buttonIcon: <Lock/>,
+        buttonIcon: <DeeplIcon/>,
         buttonLabel: 'translation-deepl:label.action',
         targets: ['content-editor/field/3dots:5.5'],
         component: RequestTranslationDeeplActionComponent
     });
 
     registry.add('action', 'requestTranslationDeeplForAllLanguages', {
-        buttonIcon: <Copy/>,
+        buttonIcon: <DeeplIcon/>,
         buttonLabel: 'translation-deepl:label.actionAllProperties',
         targets: ['content-editor/header/3dots:5.5'],
-        component: RequestTranslationDeeplForAllLanguagesActionComponent
-    });
-
-    registry.add('action', 'requestContentTranslationDeeplForAllLanguages', {
-        buttonIcon: <Copy/>,
-        buttonLabel: 'translation-deepl:label.actionAllProperties',
-        showOnNodeTypes: ['jnt:page', 'jnt:content'],
         component: RequestTranslationDeeplForAllLanguagesActionComponent
     });
 };
